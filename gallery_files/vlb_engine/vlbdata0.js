ccd66ea869c0170690a20e086b9d8d329a9a5ece
yangdahui <yanghui.hit@gmail.com>
2014-12-14 13:44:23 +0100

    initial personal website
    remove hzy115 from penn state personal website

---
+++ gallery_files/vlb_engine/vlbdata0.js
@@ -0,0 +1,1 @@
+jQuery(document).ready(function(){ window.Lightbox = new jQuery().visualLightbox({autoPlay:true,borderSize:10,classNames:'vlightbox0',closeLocation:'top',descSliding:false,enableRightClick:false,enableSlideshow:true,pauseLocation:'imageContainerMain',prefix:'vlb0',resizeSpeed:7,slideTime:4,startZoom:true}) });
